c88dde6df00144b1573b95d8b1caa28cb4749e93
Gabriel Kutsovsky <gkutsovsky@gmail.com>
2019-06-10 11:36:16 -0400

    minor changes :

--- src/resumeData.js
+++ src/resumeData.js
@@ -69,8 +69,8 @@ let resumeData = {
       "url":"https://miamiridez.netlify.com/"
     },
     {
-      "name":"Perfect Pairings",
-      "description":"Web Application, First Project at Wyncode. Used third party API to fetch marijuana strains to pair with different food item",
+      "name":"Brew Finder",
+      "description":"Web Application, First Project at Wyncode. Used third party API to fetch local breweries in your city",
       "imgurl": "images/portfolio/beautify.jpg",
       "url":"https://perfectpairings.netlify.com/",
       
